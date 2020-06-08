378055f064e8464d5dc16306cbf87ecd4fe80997
Arpan <arpanchowdhury050@gmail.com>
2020-06-08 21:08:59 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+window.addEventListener('load' ,() => {
+    const sounds = document.querySelectorAll('.audio');
+    const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector('.visual');
+
+    const colors = [
+        "#60d394",
+        "#d36060",
+        "#c060d3",
+        "#d3d160",
+        "#606bd3",
+        "#60c2d3"
+    ];
+
+    console.log(sounds);
+    
+    pads.forEach((pad, index) => {
+        pad.addEventListener('click', function() {
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+
+            createBubbles(index);
+        });
+    });
+
+    // Function that makes bubbles
+    const createBubbles = (index) => {
+        const bubble = document.createElement('div');
+        visual.appendChild(bubble);
+        bubble.style.background = colors[index];
+        bubble.style.animation = 'jump 1s ease'
+        bubble.addEventListener('animationend', function(){
+            visual.removeChild(this);
+        })
+    }
+});
+
